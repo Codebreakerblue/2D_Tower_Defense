0f4bdd8e1f8774e6537510d645e74f434eb9658a
Codebreakerblue <thecodebreakerblue@gmail.com>
2023-09-17 00:01:46 -0600

    added 'exit to menu' button

--- Scenes/MainScenes/SceneHandler.gd
+++ Scenes/MainScenes/SceneHandler.gd
@@ -1,15 +1,26 @@
 extends Node
 
 func _ready():
-	get_node("MainMenu/Margin/VBox/NewGameButton").connect("pressed", onNewGamePressed)
-	get_node("MainMenu/Margin/VBox/QuitButton").connect("pressed", onQuitPressed)
+	load_main_menu()
 
-func onNewGamePressed():
+func load_main_menu():
+	get_node("MainMenu/Margin/VBox/NewGameButton").connect("pressed", on_new_game_pressed)
+	get_node("MainMenu/Margin/VBox/QuitButton").connect("pressed", on_quit_pressed)
+
+func on_new_game_pressed():
 	get_node("MainMenu").queue_free()
 #	print("New Game pressed!")
 	var game_scene = load("res://Scenes/MainScenes/game_scene.tscn").instantiate()
 	add_child(game_scene)
+	game_scene.get_node("UI/HUD/Margin/ExitGameButton").connect("pressed", on_exit_game_pressed)
 
-func onQuitPressed():
+func on_exit_game_pressed():
+	get_node("GameScene").queue_free()
+	print("Exit Game Pressed!")
+	var main_menu = load("res://Scenes/UIScenes/main_menu.tscn").instantiate()
+	add_child(main_menu)
+	load_main_menu()
+
+func on_quit_pressed():
 	get_tree().quit()
 
